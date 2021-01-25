636f5650eddd029f3c9be10459c1a546148f5985
jovial-application-1943 <fatima.katrib@lau.edu>
2021-01-25 20:13:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Your code goes here
   document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");});
-
-// function changeText(){
-
-
-// // document.getElementById('text').innerHTML="This is really cool!"
-// }
+function updateDOM() {
+  document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
+}
